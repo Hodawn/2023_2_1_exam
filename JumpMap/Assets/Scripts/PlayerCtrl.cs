a86dee43e92c54f3f5d048661bc32d35e9e41772
Hodawn <happygy925@naver.com>
2023-10-15 14:27:36 +0900

    Test : "input시스템 테스트"

--- JumpMap/Assets/Scripts/PlayerCtrl.cs
+++ JumpMap/Assets/Scripts/PlayerCtrl.cs
@@ -3,27 +3,19 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerCtrl : MonoBehaviour
 {
-
-    private Vector3 moveDirection;
-    private float moveSpeed = 4f;
-
-    private void Update()
-    {
-        bool hascontrol = (moveDirection != Vector3.zero);
-        if (hascontrol)
-        {
-            transform.rotation = Quaternion.LookRotation(moveDirection);
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        }
-    }
+   
+    
     void OnMove(InputValue value)
     {
-        Vector2 input = value.Get<Vector2>();
-        if(input != null)
-        {
-            moveDirection = new Vector3(input.x, 0f, input.y);
-            
-        }
+        Vector2 dir = value.Get<Vector2>();
+        Debug.Log($"Move = ({dir.x},{dir.y})");
     }
-    
-   
+    void OnAttack()
+    {
+        Debug.Log("Attack");
+    }
+    void OnJump()
+    {
+        Debug.Log("Jump");
+    }
+}
