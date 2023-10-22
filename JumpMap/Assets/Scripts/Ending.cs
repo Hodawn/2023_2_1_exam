19eb2a85b759e5cb0f033bdb44b1dbab0b69df25
Hodawn <happygy925@naver.com>
2023-10-22 22:45:53 +0900

    Feat : "엔딩 완성"

---
+++ JumpMap/Assets/Scripts/Ending.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // 씬 관리 기능 사용
+
+public class Ending : MonoBehaviour
+{
+    public string EndingScene; // 엔딩 씬 이름
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // 플레이어와 충돌한 경우
+        {
+            // 엔딩 씬으로 전환
+            SceneManager.LoadScene(EndingScene);
+        }
+    }
+}
